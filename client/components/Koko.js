67ee9e86f06010db891be5f74e840b30f7ebf453
Krzysztof <musashimiyamoto@interia.eu>
2018-03-11 14:09:51 +0100

    mod10zad_2

---
+++ client/components/Koko.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+const Koko = (props, context) => {
+  return (
+  );
+};
+
+Koko.propTypes = {
+};
+
+export default Koko;
--- server/models/post.js
+++ server/models/post.js
@@ -8,6 +8,9 @@ const postSchema = new Schema({
   slug: { type: 'String', required: true },
   cuid: { type: 'String', required: true },
   dateAdded: { type: 'Date', default: Date.now, required: true },
+  thumbUp: { type: 'Number', default: 0, required: true },
+  thumbDown: { type: 'Number', default: 0, required: true },
+  votes: { type: 'Number', default: 0 }
 });
 
 export default mongoose.model('Post', postSchema);
